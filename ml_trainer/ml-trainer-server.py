e9467ed3aea6d0fc69403579febcb253ad4ece2a
PenHsuanWang <willy118116@gmail.com>
2022-09-26 11:50:34 +0800

    implement model training workflow and corresponding api tester

--- ml_trainer/ml-trainer-server.py
+++ ml_trainer/ml-trainer-server.py
@@ -1,8 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
 from flask import Flask, request, Response, abort
 import time
+import json
+from json import JSONEncoder
 import traceback
 
 import pandas
+import numpy
 
 from data_loader.data_loader import CsvDataLoader
 
@@ -14,6 +18,8 @@ class ModelTrainerServer:
     def __init__(self):
         self._data_loader = None
         self._model = None
+        self._df_x = None
+        self._y = None
 
         self.app = Flask(__name__)
 
@@ -28,14 +34,26 @@ class ModelTrainerServer:
 
         try:
             data_path = request.get_json()['data_path']
+            label_name = request.get_json()['label']
             print("target data path is {}".format(data_path))
 
             if data_path.split(".")[-1] == 'csv':
                 print("data format is csv")
                 self._data_loader = CsvDataLoader(data_path=data_path)
-                df = self._data_loader.get_df(do_label_encoder=True)
-                print(df.head(5))
+
+                self._df_x = self._data_loader.get_df(do_label_encoder=True)
+                self._y = self._df_x.pop(label_name)
+
+                print(self._df_x.head(5))
                 print("load data success")
+
+                return Response(
+                    json.dumps(
+                        'load {} data success'.format(data_path)
+                    ),
+                    status=200,
+                    headers={'content-type': 'application/json'}
+                )
 
         except:
             traceback.print_exc()
@@ -44,34 +62,116 @@ class ModelTrainerServer:
             status=200
         )
 
+    def check_data(self):
+        print(self._df_x.head(5))
+        print(self._y.head(5))
+
+        return Response(
+            json.dumps(
+                str(self._df_x.head(5)) +
+                '\n' +
+                str(self._y.head(5))
+            ),
+            status=200,
+            headers={'content-type':'application/json'}
+        )
+
+    def init_model(self):
+
+        print("going to initialized the model")
+
+        try:
+            model_type = request.get_json()['model_type']
+            model_params = request.get_json()['model_params']
+
+            print(model_type)
+            print(model_params)
+
+            model = None
+
+            if model_type == 'sklearn random forest classifier':
+
+                try:
+                    model = RandomForestClassifier(
+                        **model_params
+                    )
+                    print(model)
+                except:
+                    traceback.print_exc()
+
+            if model is not None:
+                self._model = model
+                print("finish of model initialization")
+
+                return Response(
+                    json.dumps("finish of model initialization"),
+                    status=200,
+                    headers={'content-type': 'application/json'}
+                )
+
+        except:
+            traceback.print_exc()
+
+
+    def fit(self):
+
+        print("going to fit the model")
+
+        try:
+            self._model.fit(self._df_x, self._y)
+            print("trained model successfully")
+
+            return Response(
+                status=200
+            )
+
+        except:
+            traceback.print_exc()
+            print('Unexpect error happen when doing model fitting')
+
+
+    def predict(self):
+
+        class NumpyArrayEncoder(JSONEncoder):
+            def default(self, obj):
+                if isinstance(obj, numpy.ndarray):
+                    return obj.tolist()
+                return JSONEncoder.default(self, obj)
+
+        print("going to do data prediction")
+
+        try:
+            data = request.get_json()
+
+            data = json.dumps(data)
+            df = pandas.read_json(data)
+            print(df)
+            print(df.head())
+
+            result = self._model.predict(df)
+            print(result)
+            return Response(
+                json.dumps(result, cls=NumpyArrayEncoder),
+                status=200,
+                headers={'content-type': 'application/json'}
+            )
+
+        except:
+            traceback.print_exc()
+
+
 
     def run(self):
         self.app.add_url_rule(rule='/', endpoint='ml_training_message', view_func=self.ml_trainer_hello_message)
         self.app.add_url_rule(rule='/load-data/', endpoint='load_data', view_func=self.load_data, methods=['POST'])
+        self.app.add_url_rule(rule='/check-data/', endpoint='check_data', view_func=self.check_data, methods=['POST'])
+        self.app.add_url_rule(rule='/init-model/', endpoint='init_model', view_func=self.init_model, methods=['POST'])
+        self.app.add_url_rule(rule='/fit/', endpoint='fit', view_func=self.fit, methods=['POST'])
+        self.app.add_url_rule(rule='/predict/', endpoint='predict', view_func=self.predict, methods=['POST'])
         print("Start flask server")
         self.app.run()
 
 
-    #
-    # @app.route('/init-model')
-    # def init_model():
-    #     print("constructing model")
-    #     time.sleep(1)
-    #     print("finished of model initialization.")
-    #
-    #     return Response(
-    #         status=200
-    #     )
-    #
-    # @app.route('/fit')
-    # def fit():
-    #     print("model fitting")
-    #     time.sleep(10)
-    #     print("finished of model training")
-    #
-    #     return Response(
-    #         status=200
-    #     )
 
 
 
--- tests/test_api_request_signal.py
+++ tests/test_api_request_signal.py
@@ -1,15 +1,96 @@
+import pandas as pd
+import numpy as np
+import json
 import requests
 import traceback
+from data_loader.data_loader import CsvDataLoader
+from sklearn.metrics import accuracy_score
 
 def test_loader_data_api():
 
     try:
         response = requests.post(
             url='http://127.0.0.1:5000/load-data/',
-            data='{"data_path":"/Users/pwang/BenWork/OnlineML/onlineml/data/airline/airline_data.csv"}',
+            data='{"data_path":"/Users/pwang/BenWork/OnlineML/onlineml/data/airline/airline_data.csv","label":"satisfaction"}',
             headers={'content-type': 'application/json'}
         )
         print(response.json())
         assert True
     except:
         traceback.print_stack()
+
+def test_check_data_api():
+
+    try:
+        response = requests.post(
+            url='http://127.0.0.1:5000/check-data/',
+        )
+        print(response.json())
+        assert True
+    except:
+        traceback.print_stack()
+
+def test_init_model_api():
+
+    try:
+
+        data = {
+            "model_type": "sklearn random forest classifier",
+            "model_params": {"n_estimators": "10","criterion": "gini","verbose": "1"}
+
+        }
+
+        model_params = {"n_estimators": "10","criterion": "gini","verbose": "1"}
+        #'{"model_type":"sklearn random forest classifier", "model_params":"{"n_estimators": "10","criterion": "gini","verbose": "1"}"}'
+        response = requests.post(
+            url='http://127.0.0.1:5000/init-model/',
+            data='{"model_type":"sklearn random forest classifier","model_params":{"n_estimators":10,"criterion":"gini","verbose":1}}',
+            headers={'content-type': 'application/json'}
+        )
+        print(response.json())
+        assert True
+    except:
+        traceback.print_exc()
+
+
+def test_fit_api():
+
+    try:
+        response = requests.post(
+            url='http://127.0.0.1:5000/fit/',
+        )
+        print(response.json())
+        assert True
+    except:
+        pass
+
+
+def test_predict_api():
+
+    df = CsvDataLoader(data_path="/Users/pwang/BenWork/OnlineML/onlineml/data/airline/airline_data.csv").get_df(do_label_encoder=True)
+    # df = pd.read_csv("/Users/pwang/BenWork/OnlineML/onlineml/data/airline/airline_data.csv")
+    df = df.head(1000)
+    y = df.pop('satisfaction')
+
+    df_to_send = str(df.to_json())
+
+
+    try:
+        response = requests.post(
+            url='http://127.0.0.1:5000/predict/',
+            data=df_to_send,
+            headers={'content-type': 'application/json'}
+        )
+        result = response.json()
+        print(result)
+        # result = json.loads(result)
+        # print(np.asarray(result['array']))
+        print(y.tolist())
+
+        acc = accuracy_score(y.tolist(), result)
+        print(acc)
+
+        assert True
+
+    except:
+        traceback.print_exc()
